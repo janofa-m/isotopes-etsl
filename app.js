a635521dfa5b503487ef399c23a0b1178df60f58
Janofa <148072751+janofa-m@users.noreply.github.com>
2024-07-29 20:44:02 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 500;
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function() {
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+});
